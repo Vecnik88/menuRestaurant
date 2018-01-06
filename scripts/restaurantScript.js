01eb75e98f908dea4629721a00dd591a5a77b088
Vecnik <Ve_cni_k@inbox.ru>
2018-01-07 00:59:33 +0700

    continue

--- scripts/restaurantScript.js
+++ scripts/restaurantScript.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
-	var v = f;
+	var state = false;
 	$("button#vegOn").click(function() {
-		if (v == false) {
-			v = true;
+		if (state === false) {
+			$f = $(".fish").parent().parent().detach();
+			$(".hamburger").replaceWith("<li class='portobello'><em>Portobello mushrum</em></li>");
+			$(".meat").after("<li class='tofu'><em>Tofu</em></li>");
+			$(".meat").detach();
+			//$(".tofu").parent().parent().addClass("veg_leaf");
+			state = true;
 		}
 	});
 	$("button#restoreMe").click(function() {
-		if (v == true) {
-			v = false;
+		if (state === false) {
+			console.log("click");
+			$(".menu_entrees li").first().before($f);
+			$(".portobello").replaceWith("<li class='hamburger'>hamburger</li>");
+			//$(".tofu").removeClass("veg_leaf");
+			state = false;
 		}
 	});
 });
